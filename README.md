0216fff4a68e25a6bb8dcbfbcb5a58efcad7adb5
EddyGlezz <eddyglezz95@gmail.com>
2022-10-27 12:14:48 +0200

    init the repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom Datasets
+
+This is intended to be an interface library to link to custom datasets for Pytorch(C++).
---
+++ include/customs_datasets.hpp
@@ -0,0 +1,9 @@
+#ifdef USE_IRIS
+#include<iris.hpp>
+#endif
+#ifdef USE_CMNIST
+#include<cmnist.hpp>
+#endif
+#ifdef USE_FP2
+#include<fp2.hpp>
+#endif
